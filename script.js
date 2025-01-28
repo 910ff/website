a55d547182a7d6fb3de365908b9dc8758357e118
910f <darwishyamor@gmail.com>
2025-01-28 15:26:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ScrollReveal().reveal('.hero-content', {
+    delay: 300,
+    distance: '50px',
+    duration: 1000,
+    opacity: 0,
+    reset: true,
+    easing: 'ease-in-out'
+});
+
+
+ScrollReveal().reveal('#about', {
+    delay: 300,
+    distance: '50px',
+    duration: 1000,
+    opacity: 0,
+    reset: true,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('#projects .card', {
+    delay: 300,
+    distance: '50px',
+    duration: 1000,
+    opacity: 0,
+    reset: true,
+    easing: 'ease-in-out',
+    interval: 200
+});
+
+// GSAP Animation for Hero Section (optional)
+gsap.from(".hero-content h1", { opacity: 0, y: -50, duration: 1 });
+gsap.from(".hero-content p", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
